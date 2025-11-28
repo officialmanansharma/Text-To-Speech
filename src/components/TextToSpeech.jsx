6b032d5dea4fd5efc8ae183c4ca047197094d42d
officialmanansharma <139957991+officialmanansharma@users.noreply.github.com>
2025-11-28 19:46:49 +0530

    Add additional heading 'Jai Mata Di' to TextToSpeech

--- src/components/TextToSpeech.jsx
+++ src/components/TextToSpeech.jsx
@@ -2,6 +2,9 @@ import React from 'react'
  
 export const TextToSpeech = () => {
   return (
-    <div><h1>Har Har Mahadev</h1></div>
+    <div>
+     <h1>Har Har Mahadev</h1>
+     <h1>Jai Mata Di</h1>
+    </div>
   )
 }
